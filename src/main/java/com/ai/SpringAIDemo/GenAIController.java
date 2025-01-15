6625dd04db22ae27a956cae6873dbe17b9d23104
221B0825 <eunseoyu0825@gmail.com>
2025-01-15 13:50:06 +0900

    feat: basic & options api

---
+++ src/main/java/com/ai/SpringAIDemo/GenAIController.java
@@ -0,0 +1,32 @@
+package com.ai.SpringAIDemo;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GenAIController {
+
+    ChatService chatService;
+
+    public GenAIController(ChatService chatService) {
+        this.chatService = chatService;
+    }
+
+    @GetMapping("ping")
+    public String ping(){
+        return "pong";
+    }
+
+    @GetMapping("ask-ai")
+    public String getResponse(@RequestParam String prompt){
+        return chatService.getResponse(prompt);
+    }
+
+    @GetMapping("ask-ai-options")
+    public String getResponseOptions(@RequestParam String prompt){
+        return chatService.getResponseOptions(prompt);
+    }
+
+}
